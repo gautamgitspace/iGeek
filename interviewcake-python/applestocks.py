116d4027b99bdfacaf613b85fd825db95d644c0d
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-12 15:39:18 -0400

    added problem statement

--- interviewcake-python/applestocks.py
+++ interviewcake-python/applestocks.py
@@ -1,3 +1,14 @@
+"""
+	*******************************************************************************************
+	Suppose we could access yesterday's stock prices as an array, where:
+	- The indices are the time in minutes past trade opening time, which was 9:30am local time.
+	- The values are the price in dollars of Apple stock at that time.
+
+	For example, if the stock cost $500 at 10:30am, stock_prices_yesterday[60] = 500.
+	Write an efficient function that takes stock_prices_yesterday and returns the
+    best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+    *******************************************************************************************
+"""
 def best_profit(stock_prices_yesterday):
     max_profit = 0
     for outer in xrange(len(stock_prices_yesterday)):
