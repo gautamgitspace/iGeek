25ca8b256824df3693cf484064fa140a196c57db
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-12 16:56:59 -0400

    product except index

---
+++ interviewcake-python/product-except.py
@@ -0,0 +1,15 @@
+def get_products_of_all_ints_except_at_index(list_of_numbers):
+    resultant_array = [1] * len(list_of_numbers)
+    product = 1
+    for i in range(len(list_of_numbers)):
+        resultant_array[i]=product
+        product*=list_of_numbers[i]
+    product = 1
+    for i in range(len(list_of_numbers)-1, 0, -1):
+        resultant_array[i]*=product
+        product*=list_of_numbers[i]
+    resultant_array[0]=product
+    return resultant_array
+list_of_numbers = [1,2,3,4,1,2]
+result = get_products_of_all_ints_except_at_index(list_of_numbers)
+print result
