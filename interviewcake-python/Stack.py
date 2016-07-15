bc5abef337d6dac2669fa19e594ad872395fed77
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-15 17:32:26 -0400

    largest-stack: Stack class

---
+++ interviewcake-python/Stack.py
@@ -0,0 +1,23 @@
+class Stack:
+
+    # initialize an empty list
+    def __init__(self):
+        self.items = []
+
+    # push a new item to the last index
+    def push(self, item):
+        self.items.append(item)
+
+    # remove the last item
+    def pop(self):
+
+        # if the stack is empty, return None
+        # (it would also be reasonable to throw an exception)
+        if not self.items: return None
+
+        return self.items.pop()
+
+    # see what the last item is
+    def peek(self):
+        if not self.items: return None
+        return self.items[len(self.items)-1]
