58a73759e0ef2e5729389ce783ad52037a916162
gautamgitspace <agautam2@buffalo.edu>
2020-05-20 13:22:39 -0700

    merging two dicts - chainmap and dict.update()

---
+++ Python Advanced Topics/chainmap_and_update.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+chainmap: logically combine two different mappings into one. chainmap will
+search for key in first, if not found, then in second
+"""
+
+from collections import ChainMap
+
+a = {'x' : 1, 'y' : 2}
+b = {'y' : 3, 'z' : 4}
+
+c = ChainMap(a, b)
+
+print (c['x'])
+print (c['y']) # will find in a and print from a
+print (c['z']) # will NOT find in a and search and print from b
+
+print ('\n')
+
+merge = dict(a)
+merge.update(b)
+
+print(merge['x'])
+print(merge['y']) # y will be updated from b 
+print(merge['z'])
